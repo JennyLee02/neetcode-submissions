29065e3db86aa77d6e78b3b6ffd5caf93d4799ac
Jenny Lee <107929895+JennyLee02@users.noreply.github.com>
2026-09-24 01:01:19 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # two pointer
+        s = "".join(char.lower() for char in s if char.isalnum())
+        ptr_1 = 0
+        ptr_2 = len(s) -1
+
+        while ptr_1 < ptr_2:
+            if s[ptr_1] != s[ptr_2]:
+                return False
+            ptr_1 += 1
+            ptr_2 -= 1
+        
+        return True
+        
