9ae739511a4e5ba4a6a2dd8f33ea2e003be1af3d
Jenny Lee <107929895+JennyLee02@users.noreply.github.com>
2026-09-25 13:32:35 -0400

    Add: max-water-container - submission-4

---
+++ Data Structures & Algorithms/max-water-container/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        # two pointers one each end
+        # set max and update only when the maxArea changes
+        # area = lower height of two * (high - low)
+
+        low, high = 0, len(heights) - 1
+        maxArea = 0
+        for _ in range(len(heights)):
+            area = min(heights[low], heights[high]) * (high - low)
+            if area > maxArea:
+                maxArea = area
+            if heights[low] < heights[high]:
+                low += 1
+            else:
+                high -= 1
+        
+        return maxArea
